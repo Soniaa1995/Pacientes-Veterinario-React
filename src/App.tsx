7fd42f5e91a59f90a01f0e88e79e96c631cbc082
Sonia Moreno <94900792+Soniaa1995@users.noreply.github.com>
2024-08-02 10:34:25 +0200

    Creando dos componentes: formulario para que rellenen pacientes con su html y otro componente para listar a los pacientes

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import PatientForm from "./components/PatientForm"
+import PatientList from "./components/PatientList"
 
 
 function App() {
@@ -5,7 +7,20 @@ function App() {
 
   return (
     <>
-     <h1 className="font-bold text-6xl">Pacientes</h1>
+     <div className="container mx-auto mt-20">
+		<h1 className="font-bold text-5xl text-center md:w-2/3 md:mx-auto">
+			Seguimiento de pacientes {''}
+			<span className="text-indigo-700">Veterinaria</span>
+		</h1>
+
+		<div className="mt-12 md:flex">
+			<PatientForm />
+			<PatientList />
+		</div>
+
+	 </div>
+
+
     </>
   )
 }
